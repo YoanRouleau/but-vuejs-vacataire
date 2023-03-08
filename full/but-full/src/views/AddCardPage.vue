a226f0282b0dd7152a5d995e2dfa25391e0eeb9f
YoanRouleau <yoan-rouleau@live.fr>
2023-03-08 20:47:53 +0100

    image pendant creation ok

--- full/but-full/src/views/AddCardPage.vue
+++ full/but-full/src/views/AddCardPage.vue
@@ -1,86 +1,88 @@
 <script>
-  import { data } from '../store.js'
+import { data } from '../store.js'
 
-  export default{
+export default{
     data(){
-      return {
-        search: '',
-        apiUsername: import.meta.env.VITE_IMGFLIP_USERNAME,
-        apiPassword: import.meta.env.VITE_IMGFLIP_PASSWORD,
-        name: '',
-        textBox0: '',
-        textBox1: '',
-        newMeme: '',
-        matchingMemes: []
-      }
+        return {
+            search: '',
+            apiUsername: import.meta.env.VITE_IMGFLIP_USERNAME,
+            apiPassword: import.meta.env.VITE_IMGFLIP_PASSWORD,
+            name: '',
+            textBox0: '',
+            textBox1: '',
+            newMeme: '',
+            matchingMemes: []
+        }
     },
     methods: {
-      searchMeme: async function(e){
-        return fetch( "https://api.imgflip.com/get_memes" )
-            .then(response => response.json())
-            .then( ({data}) => {
-                this.matchingMemes = data.memes.filter( meme => {
-                    return this.formattedName( meme.name ).indexOf( this.formattedName(this.search) ) > -1
-                })    
-            })
-            .catch(error => {
-                console.log(error)
-            })
-      },
-      addMeme: async function(){
-        var selectedMeme = document.querySelector('option')
+        searchMeme: async function(e){
+            return fetch( "https://api.imgflip.com/get_memes" )
+                .then(response => response.json())
+                .then( ({data}) => {
+                    this.matchingMemes = data.memes.filter( meme => {
+                        return this.formattedName( meme.name ).indexOf( this.formattedName(this.search) ) > -1
+                    })    
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        addMeme: async function(){
+            var selectedMeme = document.querySelector('option')
 
-        const options = {
-          method: 'POST',
-          headers: {
-            cookie: 'claim_key=M09Hir332NYnO9N0RfJLn09eIhB6bhgl',
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          body: new URLSearchParams({
-            username: this.apiUsername,
-            password: this.apiPassword,
-            text0: this.textBox0 || ' ',
-            text1: this.textBox1 || ' ',
-            template_id: parseInt(selectedMeme.getAttribute('data-id'))
-          })
+            const options = {
+                method: 'POST',
+                headers: {
+                    cookie: 'claim_key=M09Hir332NYnO9N0RfJLn09eIhB6bhgl',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    username: this.apiUsername,
+                    password: this.apiPassword,
+                    text0: this.textBox0 || ' ',
+                    text1: this.textBox1 || ' ',
+                    template_id: parseInt(selectedMeme.getAttribute('data-id'))
+                })
+            }
+
+            const fetchRes = await fetch('https://api.imgflip.com/caption_image', options)
+            console.log(fetchRes)
+            const fetchData = await fetchRes.json()
+            data.memes.push({
+                name: selectedMeme.value,
+                id: parseInt(selectedMeme.getAttribute('data-id')),
+                imgSrc: fetchData.data.url
+            })
+            
+            alert("Your Meme has been created!")
+        },
+        formattedName: function(string){
+            return string.toLowerCase()
         }
-
-        const fetchRes = await fetch('https://api.imgflip.com/caption_image', options);
-        console.log(fetchRes);
-        const fetchData = await fetchRes.json()
-        data.memes.push({
-            name: selectedMeme.value,
-            id: parseInt(selectedMeme.getAttribute('data-id')),
-            imgSrc: fetchData.data.url
-        })
-        alert("Your Meme has been created!")
-      },
-      formattedName: function(string){
-        return string.toLowerCase()
-      }
     },
     mounted(){
-        console.log(import.meta.env)
-        const opts = document.getElementById('selected-meme').childNodes;
-        const dinput = document.getElementById('search');
-        let eventSource = null;
-        let value = '';
+        const opts = document.getElementById('selected-meme').childNodes
+        const dinput = document.getElementById('search')
+        let eventSource = null
+        let value = ''
         dinput.addEventListener('keydown', (e) => {
-            eventSource = e.key ? 'input' : 'list';
-        });
+            eventSource = e.key ? 'input' : 'list'
+        })
         dinput.addEventListener('input', (e) => {
-            value = e.target.value;
+            value = e.target
             if (eventSource === 'list') {
-                console.log(value)
+                setTimeout(function(){
+                    document.getElementById('output').src = document.querySelector('option').getAttribute('data-url')
+                }, 100)
             }
-        });
+        })
     }
-  } 
+}
 </script>
 
 <template>
-  <main>
-    <section class="meme-editor">
+  <main class="meme-editor">
+    <section>
         <RouterLink to="/" class="meme-goback">⬅️ GO Back</RouterLink>
         <div>
             Search for a meme first: <input type="text" name="search" id="search" v-model="search" list="selected-meme" @keyup="searchMeme">
@@ -88,21 +90,35 @@
                 <option v-for="meme in matchingMemes" :value="meme.name" :data-id="meme.id" :data-url="meme.url">{{ meme.url }}</option>
             </datalist>
         </div>
+        <br>
         <p>New <strong>top</strong> text: <input type="text" v-model="textBox0"></p>
         <br>
         <p>New <strong>bottom</strong> text: <input type="text" v-model="textBox1"></p>
+        <br>
         <button @click="addMeme" class="meme-button" :disabled="textBox0.length == 0 && textBox1.length == 0">Create Meme</button>
-        <img id="output" src="">
+    </section>
+    <section>
+        <aside>
+            <img id="output" src="">
+        </aside>
     </section>
   </main>
 </template>
 
 <style>
-  .meme-editor{
-    padding: 40px;
-  }
-  .meme-goback {
-    display: block;
-    margin-bottom: 20px;
-  }
+    .meme-editor{
+        padding: 40px;
+        display: flex;
+        gap: 50px;
+    }
+    .meme-goback {
+        display: block;
+        margin-bottom: 20px;
+    }
+    #output{
+        max-width: 200px;
+        height: auto;
+        padding: 10px;
+        background-color: #F8F8F8;
+    }
 </style>
